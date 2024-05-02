ead6daf1c9a6357e36c86b8f66596574f74d2696
addmati <audtn0409@naver.com>
2024-05-02 17:52:43 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace test1
+{
+    class Program
+    {
+        static int Sum(int[] arr)
+        {
+
+            int Sum = arr[0] + arr[1] + arr[2] + arr[3];
+
+
+            return Sum;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] ints = { 3, 6, 7, 9 };
+            Console.WriteLine(Sum(ints));
+        }
+    }
+}
